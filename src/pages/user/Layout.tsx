3baf11ffd7013cba4287f46ad8212597ddfa774d
rektgit2024 <myemailispatrick@gmail.com>
2024-09-10 10:15:58 +0800

    refactored Layout and added basic footer and header component

--- src/pages/user/Layout.tsx
+++ src/pages/user/Layout.tsx
@@ -1,12 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import Header from "../../components/user/Navbar/Header";
+import Footer from "../../components/user/Footer/Footer";
 
 const Layout = () => {
+  const gridTemplateColumns = useBreakpointValue({
+    base: "1fr",
+    md: "0.3fr 1fr 0.3fr",
+  });
+
+  const gridTemplateAreas = useBreakpointValue({
+    base: `"content1"`,
+    md: `"sidebar content1 sidebar1"`,
+  });
   return (
-    <Box padding={5}>
-      <Outlet />
-      <ScrollRestoration />
-    </Box>
+    <>
+      <Header />
+      <Grid
+        templateColumns={gridTemplateColumns}
+        templateAreas={gridTemplateAreas}
+        padding={{ base: 5, md: 10, lg: 15, xl: 20 }}
+      >
+        <GridItem area="content1" as="section">
+          <Box padding={5}>
+            <Outlet />
+            <ScrollRestoration />
+          </Box>
+        </GridItem>
+      </Grid>
+      <Footer />
+    </>
   );
 };
 
