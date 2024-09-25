24145b7cf36bbaf9c1901d646a18cf34dbd7e65a
rektgit2024 <myemailispatrick@gmail.com>
2024-09-25 22:31:22 +0800

    refactored the ProfilePageHeader component

--- src/components/ProfilePage/ProfilePageHeader.tsx
+++ src/components/ProfilePage/ProfilePageHeader.tsx
@@ -6,6 +6,7 @@ import {
   Grid,
   GridItem,
   Show,
+  Spacer,
   Text,
   useBreakpointValue,
   useColorMode,
@@ -23,6 +24,8 @@ const ProfilePageHeader = () => {
     base: `"header"`,
     xl: `"asideLeft header asideRight"`,
   });
+
+  const isSmallScreen = useBreakpointValue({ base: true, lg: false });
   return (
     <Card>
       <Grid
@@ -32,7 +35,7 @@ const ProfilePageHeader = () => {
         <GridItem area="header">
           <Box
             width="100%"
-            height={{ md: "300px", lg: "400px", xl: "450px" }}
+            height={{ base: "250px", md: "330px", lg: "400px", xl: "450px" }}
             bg={colorMode === "dark" ? "#181818" : "gray.100"}
             _hover={{ bg: colorMode === "dark" ? "#282828" : "gray.200" }}
             cursor="pointer"
@@ -50,21 +53,30 @@ const ProfilePageHeader = () => {
               borderBottomLeftRadius="10px"
               borderBottomRightRadius="10px"
             /> */}
-            <Box display="flex" justifyContent="end" mb="20px" mr="30px">
+            <Box
+              display="flex"
+              justifyContent="end"
+              mb="20px"
+              mr={{ base: "10px", md: "30px" }}
+            >
               <Button color="black" bg="white" _hover={{ bg: "white" }}>
-                <FaCamera size="15px" />
-                <Text ml="5px">Add Cover Photo X</Text>
+                <FaCamera size={isSmallScreen ? "20px" : "15px"} />
+                {isSmallScreen ? "" : <Text ml="5px">Add Cover Photo X</Text>}
               </Button>
             </Box>
           </Box>
 
           <Box
             display="flex"
-            justifyContent="space-between"
-            alignItems="center"
+            alignItems={{ base: "center", lg: "flex-start" }}
+            flexDirection={isSmallScreen ? "column" : "row"}
           >
             <Box display="flex">
-              <Box position="relative" bottom="25px" left="30px">
+              <Box
+                position="relative"
+                bottom={{ base: "100px", lg: "25px" }}
+                left={{ base: "20px", md: "30px" }}
+              >
                 <Avatar
                   src="https://img.freepik.com/premium-photo/cooltrendy-funky-boy-cartoon-character-with-retro-style-design_960782-109604.jpg"
                   borderWidth="6px"
@@ -83,41 +95,61 @@ const ProfilePageHeader = () => {
                 justifyContent="center"
                 alignItems="center"
                 position="relative"
-                top="100px"
+                top={{ base: "15px", md: "25px", lg: "100px" }}
                 right="14px"
                 cursor="pointer"
               >
                 <FaCamera size="20px" />
               </Box>
-              <Box ml="40px" mt="20px">
-                <Text fontSize="xx-large" fontWeight="bold">
-                  X-NAME HERE
-                </Text>
-                <Text
-                  fontSize="md"
-                  color="gray.500"
-                  fontWeight="semibold"
-                  mb="10px"
-                >
-                  X friends and avatar
-                </Text>
-                <Avatar
-                  src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258193-stock-illustration-anonymous-business-man-icon.jpg"
-                  size="sm"
-                />
-              </Box>
             </Box>
-            <Box mt="50px" mr="30px">
-              <Button mr="7px" colorScheme="blue">
+
+            <Box
+              ml={{ base: "0px", lg: "10px" }}
+              mt={{ base: "0px", lg: "20px" }}
+              textAlign={{ base: "center", lg: "start" }}
+              position={{ base: "relative", lg: "static" }}
+              bottom="90px"
+            >
+              <Text fontSize="xx-large" fontWeight="bold">
+                X-NAME HERE
+              </Text>
+              <Text
+                fontSize="md"
+                color="gray.500"
+                fontWeight="semibold"
+                mb="10px"
+              >
+                X friends and avatar
+              </Text>
+              <Avatar
+                src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258193-stock-illustration-anonymous-business-man-icon.jpg"
+                size="sm"
+              />
+            </Box>
+            <Spacer />
+            <Box
+              mt={{ md: "20px", lg: "95px" }}
+              mr={{ base: "0px", lg: "30px" }}
+              textAlign={{ base: "center", lg: "start" }}
+              position={{ base: "relative", lg: "static" }}
+              bottom={{ base: "70px", md: "90px", lg: "none" }}
+            >
+              <Button
+                mr="7px"
+                colorScheme="blue"
+                ml={{ base: "10px", md: "0px" }}
+              >
                 <FaPlus size="15px" />
-                <Text ml="5px">Add to Story X</Text>
+                <Text ml="5px">Add to Story</Text>
               </Button>
               <Button mr="7px">
                 <MdModeEdit size="20px" />
-                <Text ml="5px">Edit profile X</Text>
+                <Text ml="5px">Edit profile</Text>
               </Button>
-              <Button>
-                X
+              <Button
+                width={{ base: "80%", md: "0" }}
+                mt={{ base: "10px", md: "0px" }}
+              >
                 <FaChevronDown />
               </Button>
             </Box>
